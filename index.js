7fae412440a1272988fd4122cc18685d50c7b156
Nivetha <nivedha.orton@gmail.com>
2019-08-14 09:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians,instruments){
   var empty = []
   for(let i=0 ; i < musicians.length ; i++){
-    name = "John Lennon plays guitar"
-    empty.push(name)
+    
+    empty.push(musicians[i]+"plays"+instruments[i])
+    
   }
+  return empty
 }// add solution here
