ef0d249f87728f622fb3c18c13eba6f1ff96c977
Nivetha <nivedha.orton@gmail.com>
2019-08-14 08:57:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var empty = []
+  for(let i=0 ; i < musicians.length ; i++){
+    name = "John Lennon plays guitar"
+    empty.push(name)
+  }
+}// add solution here
