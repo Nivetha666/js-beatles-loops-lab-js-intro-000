7950a8ad4f93f29e81b9606ab69d8c599637f39b
Nivetha <nivedha.orton@gmail.com>
2019-08-14 09:00:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians,instruments){
   var empty = []
   for(let i=0 ; i < musicians.length ; i++){
-    
+
     empty.push(musicians[i]+"plays"+instruments[i])
-    
+
   }
   return empty
 }// add solution here
